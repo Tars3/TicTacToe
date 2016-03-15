d6650dccf60fedda42d1d720b11ff3cbb25029e2
Carlos Borges <caborges3@gmail.com>
2016-03-14 18:43:35 -0700

    Getting on track

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,35 +1,92 @@
 console.log("Is this thing on?")
 
-//function startGame() {
-//  document.turn = "X"
-//}
-//function nextMove(square) {
-//  if (square.innerHTML == "") {
-//    square.innerHTML = document.turn;
-//    switchTurn();
-//  } else {
-//    alert("Hey! Someone's already there!")
-//  }
-//}
+//getElementById("b1")
+//getElementById("b2")
+//getElementById("b3")
+//getElementById("b3")
+// create objects for all the dom elements
 
-//function switchTurn() {
-//  if (document.turn =="X") {
-//    else {
-//      document.turn =="O"
-//    }
-//  }
-//}
+var b1 = document.getElementById("b1")
+var b2 = document.getElementById("b2")
+var b3 = document.getElementById("b3")
+var b4 = document.getElementById("b4")
+var b5 = document.getElementById("b5")
+var b6 = document.getElementById("b6")
+var b7 = document.getElementById("b7")
+var b8 = document.getElementById("b8")
+var b9 = document.getElementById("b9")
+var reset = document.getElementById("reset")
 
-//function getWinner(move) {
-//  if (checkRow()
-//}
+// set appropriate click handlers
+b1.addEventListener("click", mark)
+b2.addEventListener("click", mark)
+b3.addEventListener("click", mark)
+b4.addEventListener("click", mark)
+b5.addEventListener("click", mark)
+b6.addEventListener("click", mark)
+b7.addEventListener("click", mark)
+b8.addEventListener("click", mark)
+b9.addEventListener("click", mark)
 
-function clickBtn(btn) {
-  if (player == 1) {
-    document.getElementById(btn).value = "X";
-    player -= 1;
-  } else {
-    document.getElementById(btn).value = "O";
-    player += 1;
-  }
+function mark() {
+//put an X inside this element
+  this.innerHTML = "X";
+//change player turn
+  if ()
+  console.log("Pizza rocks.")
+  console.log("another string")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //function startGame() {
+// //  document.turn = "X"
+// //}
+// //function nextMove(square) {
+// //  if (square.innerHTML == "") {
+// //    square.innerHTML = document.turn;
+// //    switchTurn();
+// //  } else {
+// //    alert("Hey! Someone's already there!")
+// //  }
+// //}
+// //
+// //function switchTurn() {
+// //  if (document.turn =="X") {
+// //    else {
+// //      document.turn =="O"
+// //    }
+// //  }
+// //}
+// //
+// //function getWinner(move) {
+// //  if (checkRow()
+// //}
+// //function clickBtn(btn) {
+// //  if (player == 1) {
+// //    document.getElementById(btn).value = "X";
+// //    player -= 1;
+// //  } else {
+// //    document.getElementById(btn).value = "O";
+// //    player += 1;
+// //  }
+// //}
